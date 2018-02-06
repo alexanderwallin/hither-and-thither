ddb2c210882b715a98ea2d3eef51d57db8e98d04
Alexander Wallin <office@alexanderwallin.com>
2018-02-06 12:53:04 +0100

    Mocks up a basic getScrollState() function

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const Axis = {
+  X: 'X',
+  Y: 'Y',
+}
+
+export const Direction = {
+  UP: 'UP',
+  DOWN: 'DOWN',
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT',
+  NONE: 'NONE',
+}
+
+const defaultState = {
+  direction: {
+    x: null,
+    y: null,
+  },
+  dx: 0,
+  dy: 0,
+}
+
+function getDirection(axis, movement) {
+  if (movement > 0) {
+    return axis === Axis.X ? Direction.RIGHT : Direction.DOWN
+  } else if (movement < 0) {
+    return axis === Axis.X ? Direction.LEFT : Direction.UP
+  }
+  return Direction.NONE
+}
+
+function getDeltaState(a, b) {
+  const dx = a.scrollLeft - b.scrollLeft
+  const dy = a.scrollTop - b.scrollTop
+  const direction = {
+    x: getDirection(Axis.X, dx),
+    y: getDirection(Axis.Y, dy),
+  }
+
+  return { dx, dy, direction }
+}
+
+export function getScrollState(oldState, scrollEvent) {
+  const comparisonState = oldState || defaultState
+  const deltaState = getDeltaState(comparisonState, scrollEvent)
+
+  return {
+    ...scrollEvent,
+    ...deltaState,
+  }
+}
